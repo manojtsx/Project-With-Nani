2e94fdda06860d4bfdaebf26683af7c4b4ab6105
Manoj Shrestha <shresthanewar678@gmail.com>
2024-11-04 19:03:26 +0545

    feat:created login form

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 function Login() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  function handleVisibility() {
+    setIsVisible(!isVisible);
+  }
+  function handleInputChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setUser({ ...user, [name]: value });
+  }
+  function handleLoginForm(event) {
+    event.preventDefault();
+    if (user.username === "admin" && user.password === "admin") {
+      window.alert("You have successfully logged in..");
+    } else {
+      window.alert("Wrong Credentials.");
+    }
+  }
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gray-200">
-      <form className="bg-blue-400 rounded-xl px-10 py-14 text-white">
+      <form
+        className="bg-blue-400 rounded-xl px-10 py-14 text-white"
+        onSubmit={handleLoginForm}
+      >
         <div className="flex justify-center pb-4">
           <h1 className="text-4xl">Login Form</h1>
         </div>
@@ -13,17 +39,30 @@ function Login() {
           </label>
           <input
             type="text"
+            name="username"
             className="h-9 outline-none text-black px-2 border border-b-2 border-b-blue-500 focus:border-b-red-500"
+            onChange={handleInputChange}
+            value={user.username}
           />
         </div>
         <div className="flex flex-col py-6">
           <label htmlFor="password" className="text-black">
             Password:
           </label>
-          <input
-            type="password"
-            className="h-9 outline-none text-black px-2 border border-b-2 border-b-blue-500 focus:border-b-red-500"
-          />
+          <div className="relative">
+            <input
+              type={isVisible ? "text" : "password"}
+              name="password"
+              className="h-9 outline-none text-black px-2 border border-b-2 border-b-blue-500 focus:border-b-red-500"
+              onChange={handleInputChange}
+              value={user.password}
+            />
+            {isVisible ? (
+              <VisibilityIcon onClick={handleVisibility} className="absolute" />
+            ) : (
+              <VisibilityOffIcon onClick={handleVisibility} />
+            )}
+          </div>
         </div>
         <button
           type="submit"
